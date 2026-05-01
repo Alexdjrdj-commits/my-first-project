0aa9195027e0863e147d9069dd7d3d0bb53f8ae9
Alex Djr <alexdjr@Alexs-MacBook-Air.local>
2026-05-01 12:44:25 +0300

    first working project

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeText() {
+  document.querySelector("h1").textContent = "I just fixed my first bug😎";
+}
+
+function resetText() {
+  document.querySelector("h1").textContent = "Hello, I am learning coding 🚀";
+}
+
+function makeRed() {
+  document.querySelector("h1").style.color = "red";
+}
